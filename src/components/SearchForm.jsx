e9847d739e980d5120b45b034de73e412db8b784
pranshu <srivastavapranshu2@gmail.com>
2023-01-19 21:17:00 +0530

    search field

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,10 +1,13 @@
 import { useGlobalContext } from "./Context";
 const SearchForm = ()=>{
     const {setSearchTerm} = useGlobalContext();
+    const handleSearch = (value)=>{
+        setSearchTerm(value);
+    }
     return (
         <>
         <div>
-            SearchForm
+            <input type="text" placeholder="search cocktail" onChange={(e)=>{handleSearch(e.target.value)}}/>
         </div>
         </>
     )
